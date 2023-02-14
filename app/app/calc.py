40c809a9c533606ccef667a5a5b35dd1b39856e3
sumeetmate <sumeetmate@gmail.com>
2023-02-14 06:12:19 -0500

    Postgres database configuration

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+## calculator functions
+
+def add(x, y):
+    return x + y;
+
+def subtract(x,y):
+    """subtract x from y"""
+    return y - x;
---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+from  app import calc
+
+class testCalc(SimpleTestCase):
+
+    def test_add_number(self):
+        res = calc.add(5,6);
+        self.assertEqual(res, 11);
+
+    def test_subtract_number(self):
+        result = calc.subtract(10,15);
+        self.assertEqual(result,5);
