472ea29419bd5987528bf8b37df5521394d93e10
sumeetmate <sumeetmate@gmail.com>
2023-02-17 13:51:18 -0500

    New Model Recipe and its tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,8 @@
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from decimal import Decimal
+from core import models
 
 
 class ModelTests(TestCase):
@@ -46,3 +48,19 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            'testuser@example.com',
+            'password123'
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='chicken tikka',
+            description='indian no-veg delicacy',
+            time_minutes=5,
+            price=Decimal('6.50')
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
