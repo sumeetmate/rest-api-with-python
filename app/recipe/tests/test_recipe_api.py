1b138751222329278eccafc4504ca084b18d8a9c
sumeetmate <sumeetmate@gmail.com>
2023-02-17 16:31:34 -0500

    Recipe GET API - details + Test Cases

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -9,10 +9,15 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Recipe
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 
 RECIPE_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    "Create and return a recipe details url"
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 
 def create_recipe(user, **params):
@@ -77,5 +82,15 @@ class PrivateRecipeAPITests(TestCase):
         res = self.client.get(RECIPE_URL)
         recipes = Recipe.objects.filter(user=self.user)
         serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """test get recipe details."""
+        recipe = create_recipe(user=self.user)
+        serializer = RecipeDetailSerializer(recipe)
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -11,7 +11,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View to manage reciepe APIs"""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -19,3 +19,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrive recipes for authenticated users"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serilizer_class(self):
+        """return the serializer class for requests."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
